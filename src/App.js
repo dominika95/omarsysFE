027b7a5e7f72d1b3b3313bd57cf32be5d3b04280
LAPTOP-EFL73E3P\domin <dominika.k1595@gmail.com>
2022-10-04 13:50:57 +0200

    added footer with actions and header of second form

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import './App.scss';
 
 import FormName from './components/FormName';
 import Header from './components/Header';
+import FormPlanHeader from './components/FormPlanHeader';
+
+import ActionFooter from './components/ActionFooter';
 
 function App() {
   return (
     <div className="app">
       <Header/>
       <FormName/>
+      <FormPlanHeader/>
+
+      <ActionFooter/>
     </div>
   );
 }
---
+++ src/components/ActionFooter/index.js
@@ -0,0 +1,10 @@
+import './index.scss';
+
+export default function ActionFooter() {
+    return (
+        <div className="action-footer">
+            <button className="delete">Delete</button>
+            <button className="save">Save</button>
+        </div>
+    )
+}
